fedb4f899c71f0180e8414fb245502155e8db31e
1U23CS002AADITHYAN <139536409+1U23CS002AADITHYAN@users.noreply.github.com>
2025-01-29 10:40:53 +0000

    submit

--- LinkedList.java
+++ LinkedList.java
@@ -6,72 +6,83 @@ public class LinkedList {
         Node next;
 
         public Node(int data) {
-this.data = data;
-this.next = null;
+            this.data = data;
+            this.next = null;
         }
     }
 
     Node head = null;
     Node tail = null;
 
+    // Method to add a new node at the end of the list
     public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
-           tail.next = newNode;
+            tail.next = newNode;
             tail = newNode;
         }
     }
 
-public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        Scanner sc = new Scanner(System.in);
+    // Method to insert a new node at the end of the list
+    public void insertEnd(int data) {
+        Node newNode = new Node(data); // Create a new node with the given data.
         
-int pos=0;
-int data,data1;
-        System.out.println("ENTER INTEGERS TO CREATE NODES (ENTER A NON-INTEGER TO EXIT):");
-
-        while (sc.hasNext())
-        {
-            if(sc.hasNextInt())
-            {
-              data = sc.nextInt();
-             list.addNode(data);
-            }
-          else
-          {
-            String temp=sc.next();
-            System.out.println("LINKED LIST CREATED");
-            System.out.println("LINKED LIST BEFORE INSERTING");
-            list.displayList();
-            System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
-            if(sc.hasNextInt())
-            {
-            data1=sc.nextInt();
-            list.insertEnd(data1);
-            }
-            else{
-              System.out.println("Enter integers only");
-              System.exit(0);
-            }
-            System.out.println("LINKED LIST AFTER INSERTING");
-            list.displayList();
-          }
+        // If the list is empty, the new node becomes both the head and the tail.
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode; // Set the next pointer of the current tail to the new node.
+            tail = newNode;      // Update the tail to the new node.
         }
     }
-      public void insertEnd(int data) 
-    {    
-       //TYPE YOUR CODE HERE 
-    }  
-     
-public void displayList() {
+
+    // Method to display the linked list
+    public void displayList() {
         Node current = head;
         while (current != null) {
-System.out.print(current.data + " -> ");
+            System.out.print(current.data + " -> ");
             current = current.next;
         }
-System.out.println("null");
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ENTER INTEGERS TO CREATE NODES (ENTER A NON-INTEGER TO EXIT):");
+
+        while (sc.hasNext()) {
+            if (sc.hasNextInt()) {
+                int data = sc.nextInt();
+                list.addNode(data);
+            } else {
+                // Break out when a non-integer is entered
+                sc.next(); // Consume the non-integer input
+                System.out.println("LINKED LIST CREATED");
+                System.out.println("LINKED LIST BEFORE INSERTING");
+                list.displayList();
+
+                System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
+
+                if (sc.hasNextInt()) {
+                    int dataToInsert = sc.nextInt();
+                    list.insertEnd(dataToInsert);
+                } else {
+                    System.out.println("Enter integers only");
+                    break;
+                }
+
+                System.out.println("LINKED LIST AFTER INSERTING");
+                list.displayList();
+                break;
+            }
+        }
+
+        sc.close(); // Close the scanner after use
     }
 }
